02dae24d61e8f99e5738bd5f13194f70e91b24fc
Denys Izhetskyi <denys.izhetskyi@booking.com>
2024-04-03 17:50:57 +0200

    sam loh

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { sql } from "@vercel/postgres";
 
+import { escapeHtml } from '../utils/escape-html';
+
 export default async function handler(request: VercelRequest, response: VercelResponse) {
     const { rows } = await sql`SELECT id, name, count FROM likes order by id;`;
 
@@ -10,7 +12,7 @@ export default async function handler(request: VercelRequest, response: VercelRe
                 <h1>Little likes app <3</h1>
                 <form action="/api/like">
                     ${rows.length ? rows.map(row => {
-                        return `<div>${row.name}: ${row.count} <button type="submit" name="id" value="${row.id}">Like</button></div>`;
+                        return `<div>${escapeHtml(row.name)}: <code>${row.count}</code> <button type="submit" name="id" value="${row.id}">Like</button></div>`;
                     }).join("") : "No users yet :)"}
                 </form>
                 <form action="/api/add">
